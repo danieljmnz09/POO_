a5a24f88c885edb30c2adf13cead50ae904edeb9
danieljmnz09 <danieljimenez@liceovvh.cl>
2026-03-16 09:53:14 -0300

    ola

--- 03_Bucles_for/static/js/script.js
+++ 03_Bucles_for/static/js/script.js
@@ -1,6 +1,6 @@
 /*#1 */
 
-function Contador() {
+function Contador1() {
     let numeros = [];
     const container = document.getElementById('resultadoContainer')
     const resultado = document.getElementById('result1')
@@ -9,4 +9,88 @@ function Contador() {
     }
     resultado.textContent = `Contando: ${numeros.join(" - ")}`;
     container.classList.remove('d-none');
+};
+
+function Lista() {
+    let curso = ["Ana", "Diego", "Sofía", "Matias"];
+    const container = document.getElementById('resultadoContainer2')
+    const resultado = document.getElementById('result2')
+    resultado.textContent = "";
+    for (let i = 0; i < curso.length; i++) {
+        resultado.textContent += `${curso[i]}, `;
+    }
+    container.classList.remove('d-none');
+
+}
+
+function Buscador() {
+    let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+    let contadorAprobados = 0;
+    const container = document.getElementById('resultadoContainer3');
+    const resultado = document.getElementById('result3');
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] >= 4.0) {
+            contadorAprobados++
+        }
+    }
+    resultado.textContent = `${notas} `;
+    container.classList.remove('d-none');
+}
+
+function Inventario() {
+    let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+    const container = document.getElementById('resultadoContainer4');
+    const resultado = document.getElementById('result4');
+    let disponibles = [];
+    resultado.textContent = "";
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i] !== "Agotado") {
+            disponibles.push(productos[i]);
+        }
+    }
+    resultado.textContent = `productos disponibles: ${disponibles.join(" - ")}`
+    container.classList.remove('d-none')
+}
+
+function Sumando() {
+    let aportes = [1500, 2000, 500, 3000, 1000];
+    let totalRecaudado = 0;
+    const container = document.getElementById('resultadoContainer5');
+    const resultado = document.getElementById('result5');
+    for (let i = 0; i < aportes.length; i++) {
+        totalRecaudado += aportes[i];
+    }
+    resultado.textContent = `La colecta reunió un total de: ${totalRecaudado}`
+    container.classList.remove('d-none')
+}
+
+function Formateador() {
+    let asistentes = ["Carlos", "MARIA", "pedro", "LUCIA"];
+    let resultado = [];
+    const container = document.getElementById('resultadoContainer6')
+    const result = document.getElementById('result6');
+    for (let i = 0; i < asistentes.length; i++) {
+        if (i % 2 === 0) {
+            resultado.push(`${asistentes[i]} VIP`);
+        } else {
+            resultado.push(`${asistentes[i]} `);
+        }
+    }
+    result6.textContent = resultado.join(" - ");
+    container.classList.remove('d-none')
+};
+
+function Stock() {
+    let bodega = ["Lápiz", "Cuaderno", "Goma", "Cuaderno", "Regla", "Cuaderno"];
+    let articuloBuscado = prompt("Ingrese el articulo que busca: ");
+    let vecesEncontrado = 0;
+    const container = document.getElementById('resultadoContainer7');
+    const resultado = document.getElementById('result7');
+    for (let i = 0; i < bodega.length; i++) {
+        if (articuloBuscado == bodega[i]) {
+            vecesEncontrado++
+        }
+    }
+    resultado.textContent = `El artículo ${articuloBuscado} se encuentra ${vecesEncontrado} veces en la bodega`
+    container.classList.remove('d-none')
 }
